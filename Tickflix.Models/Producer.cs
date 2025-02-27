558a562daf75c9bf9d12f3d48f63bb9e6b341b3e
Ömer Kutlay <omer.kutlay@outlook.com>
2025-02-28 02:16:33 +0300

    Producer modeli oluşturuldu

---
+++ Tickflix.Models/Producer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickflix.Models
+{
+    public class Producer
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string Bio { get; set; }
+    }
+}
