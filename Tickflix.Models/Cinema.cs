ba68d573b8f6ed5f97a791e635e6d0f66a2daf47
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-01 17:22:11 +0300

    Cinema modeli oluşturuldu

---
+++ Tickflix.Models/Cinema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickflix.Models
+{
+    public class Cinema
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Logo { get; set; }
+    }
+}
