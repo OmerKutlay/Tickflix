09889e2cbff900ff5ba1548d6d0a3d8eb504c197
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-02 00:27:23 +0300

    Movie modeli oluşturuldu

---
+++ Tickflix.Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickflix.Models.Data;
+
+namespace Tickflix.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
