deaa76f85369a9b3b09706218e92f0e68b310966
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-02 00:27:47 +0300

    MovieCategory için enum  oluşturuldu

---
+++ Tickflix.Models/Data/MovieCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickflix.Models.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Horror,
+        SciFi,
+        Thriller,
+        Documentary
+    }
+}
