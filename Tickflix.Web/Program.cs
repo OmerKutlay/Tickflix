4d11111ec0a170dc86f3244c9445a300d2853333
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-03 14:39:24 +0300

    Vscode ile sql bağlantısı oluşturuldu

--- Tickflix.Web/Program.cs
+++ Tickflix.Web/Program.cs
@@ -1,7 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Tickflix.Web;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<AppDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+
+
+
+
+
+
+
 
 var app = builder.Build();
 
