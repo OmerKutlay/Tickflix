14419e141e3753a832b34e25bcde08bb5716a2c4
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-05 17:43:36 +0300

    Producer Controller oluşturudu

---
+++ Tickflix.Web/Controllers/ProducerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tickflix.Web.Controllers
+{
+    public class ProducerController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducerController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var allProducers = _context.Producers.ToList();
+            return View();
+        }
+    }
+}
