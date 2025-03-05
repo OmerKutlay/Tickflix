e2ee1326fe3cef27180f76722b973566bad6141a
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-05 17:43:10 +0300

    Actor Controller oluşturudu

---
+++ Tickflix.Web/Controllers/ActorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tickflix.Web.Controllers
+{
+    public class ActorController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorController(AppDbContext context)
+        {
+               _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
