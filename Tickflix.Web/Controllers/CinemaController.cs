062c3a6dbd3ea9929bd44e93f31e448ab9718790
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-05 17:43:21 +0300

    Cinema Controller oluşturudu

---
+++ Tickflix.Web/Controllers/CinemaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tickflix.Web.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemaController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Cinemas.ToList();
+            return View();
+        }
+    }
+}
