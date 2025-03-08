d1868b679e8849e8da21cac3056ad9cb1bb7131a
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-09 00:54:35 +0300

    Actor modelinin view'u oluşturuldu

--- Tickflix.Models/Actor.cs
+++ Tickflix.Models/Actor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace Tickflix.Models
     public class Actor
     {
         public int Id { get; set; }
+        [Display(Name = "Full Name")]
         public string FullName { get; set; }
+        [Display(Name = "Profile Picture URL")]
         public string ProfilePictureURL { get; set; }
+        [Display(Name = "Biography")]
         public string Bio { get; set; }
         public ICollection<Movie> Movies { get; set; }
     }
