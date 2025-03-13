bb42edfbe4d10e20085c113e3b5f6e01d2a84c3b
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-13 16:33:13 +0300

    IRepository interface'i oluşturuldu

---
+++ Tickflix.Repository/Shared/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickflix.Repository.Shared.Abstract
+{
+    internal interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate);
+        T GetById(int id);
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+        List<T> AddRange(List<T> entities);
+        T GetFirstOrDefatult(Expression<Func<T, bool>> predicate);
+        void Save();
+    }
+}
