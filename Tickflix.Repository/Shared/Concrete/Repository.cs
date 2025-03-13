803f1a3e5989439156294ddb801c2d3ca6b3c55d
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-13 17:11:00 +0300

    Repository concrete class'ı oluşturuldu ve metotları yazıldı

---
+++ Tickflix.Repository/Shared/Concrete/Repository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Tickflix.Repository.Shared.Abstract;
+using Tickflix.Web;
+
+namespace Tickflix.Repository.Shared.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public T Add(T entity)
+        {
+            _dbSet.Add(entity);
+            Save();
+            return entity;
+        }
+
+        public List<T> AddRange(List<T> entities)
+        {
+            _dbSet.AddRange(entities);
+            Save();
+            return entities;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            Save();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dbSet;
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public T GetFirstOrDefatult(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.FirstOrDefault(predicate);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public T Update(T entity)
+        {
+            _dbSet.Update(entity);
+            Save();
+            return entity;
+        }
+    }
+}
