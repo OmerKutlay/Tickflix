441f5210bb64ac8a93dca799de8acc40d7eb3c97
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-16 00:25:51 +0300

    BusinessExtansion oluşturuldu

---
+++ Tickflix.Repository/Configuration/BusinessExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickflix.Repository.Shared.Abstract;
+using Tickflix.Repository.Shared.Concrete;
+
+namespace Tickflix.Repository.Configuration
+{
+    public static class BusinessExtension
+    {
+
+        public static void RepositoryDI(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
