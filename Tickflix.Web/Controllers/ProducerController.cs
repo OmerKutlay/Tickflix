6da5cdf2dbfe060b60aa210d83feee9919fefabf
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-20 21:50:25 +0300

    Producer controller CRUD metotları yazıldı

--- Tickflix.Web/Controllers/ProducerController.cs
+++ Tickflix.Web/Controllers/ProducerController.cs
@@ -1,19 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Tickflix.Models;
+using Tickflix.Repository.Shared.Abstract;
 
 namespace Tickflix.Web.Controllers
 {
     public class ProducerController : Controller
     {
-        private readonly AppDbContext _context;
+        private readonly IRepository<Producer> _producerService;
 
-        public ProducerController(AppDbContext context)
+        public ProducerController(IRepository<Producer> producerService)
         {
-            _context = context;
+            _producerService = producerService;
         }
         public IActionResult Index()
         {
-            var allProducers = _context.Producers.ToList();
-            return View(allProducers);
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            return Json(new { data = _producerService.GetAll() });
+        }
+        [HttpPost]
+        public IActionResult Add(Producer producer)
+        {
+            return Ok(_producerService.Add(producer));
+        }
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_producerService.GetById(id));
+        }
+        [HttpPost]
+        public IActionResult Update(Producer producer)
+        {
+            return Ok(_producerService.Update(producer));
+        }
+        [HttpPost]
+        public IActionResult Delete(Producer producer)
+        {
+            _producerService.Delete(producer);
+            return Ok();
         }
     }
 }
