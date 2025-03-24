90e6b8eeadf1c2872dfeda062eb8ef7766090776
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-25 01:03:32 +0300

    Add IActorService interface and update project references

---
+++ Tickflix.Business/Abstract/IActorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickflix.Models;
+
+namespace Tickflix.Business.Abstract
+{
+    public interface IActorService
+    {
+        Actor Add(Actor actor);
+        Actor Update(Actor actor);
+        Actor GetById(int id);
+        void Delete(int id);
+        IQueryable<Actor> GetAll();
+    }
+}
