bd656b4b74abf41417876d9d20451ccccdb41668
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-25 01:07:41 +0300

    Add ICinemaService interface with CRUD methods
    Introduced a new interface `ICinemaService` in the `Tickflix.Business.Abstract` namespace. This interface includes methods for managing `Cinema` objects: `Add`, `Update`, `GetById`, `Delete`, and `GetAll`. Added necessary `using` directives for `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`.

---
+++ Tickflix.Business/Abstract/ICinemaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickflix.Models;
+
+namespace Tickflix.Business.Abstract
+{
+    public interface ICinemaService
+    {
+        Cinema Add(Cinema cinema);
+        Cinema Update(Cinema cinema);
+        Cinema GetById(int id);
+        void Delete(int id);
+        IQueryable<Cinema> GetAll();
+    }
+}
