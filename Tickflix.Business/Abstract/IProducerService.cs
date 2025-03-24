04f1dbb6a76e7799b3976e9ad87742810a0e626b
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-25 01:07:22 +0300

    Add IProducerService interface with CRUD methods
    A new interface `IProducerService` has been added to the `Tickflix.Business.Abstract` namespace. The interface includes methods for adding, updating, retrieving by ID, deleting, and retrieving all producers. Necessary `using` directives have also been added.

---
+++ Tickflix.Business/Abstract/IProducerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickflix.Models;
+
+namespace Tickflix.Business.Abstract
+{
+    public interface IProducerService
+    {
+        Producer Add(Producer producer);
+        Producer Update(Producer producer);
+        Producer GetById(int id);
+        void Delete(int id);
+        IQueryable<Producer> GetAll();
+    }
+}
