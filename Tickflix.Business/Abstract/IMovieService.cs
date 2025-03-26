e09a7aae5de24b1501cfe57f47802eca523ed17a
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-26 18:01:30 +0300

    IMovie interface'i oluşturuldu

---
+++ Tickflix.Business/Abstract/IMovieService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickflix.Models;
+
+namespace Tickflix.Business.Abstract
+{
+    public interface IMovieService
+    {
+        Movie Add(Movie movie);
+        Movie Update(Movie movie);
+        Movie GetById(int id);
+        void Delete(Movie movie);
+        IQueryable<Movie> GetAll();
+    }
+}
