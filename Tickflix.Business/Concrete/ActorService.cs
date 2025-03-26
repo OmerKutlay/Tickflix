b097b4877c4a8b0c6627cebd102d72acc4ba7bc8
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-27 00:16:58 +0300

    Add ActorService and update project references
    Added ActorService.cs to Tickflix.Business, implementing IActorService with repository pattern for managing Actor entities. Modified Tickflix.Business.csproj to include reference to Tickflix.Repository and removed Concrete folder ItemGroup. Updated Tickflix.sln to change Tickflix.Business project GUID and adjusted solution configuration platforms accordingly.

---
+++ Tickflix.Business/Concrete/ActorService.cs
@@ -0,0 +1,40 @@
+﻿using Tickflix.Business.Abstract;
+using Tickflix.Models;
+using Tickflix.Repository.Shared.Abstract;
+
+namespace Tickflix.Business.Concrete
+{
+    public class ActorService : IActorService
+    {
+        private readonly IRepository<Actor> _actorService;
+        public ActorService(IRepository<Actor> actorRepository)
+        {
+            _actorService = actorRepository;
+        }
+
+        public Actor Add(Actor actor)
+        {
+            return _actorService.Add(actor);
+        }
+
+        public void Delete(Actor actor)
+        {
+            _actorService.Delete(actor);
+        }
+
+        public IQueryable<Actor> GetAll()
+        {
+            return _actorService.GetAll();
+        }
+
+        public Actor GetById(int id)
+        {
+            return _actorService.GetById(id);
+        }
+
+        public Actor Update(Actor actor)
+        {
+            return _actorService.Update(actor);
+        }
+    }
+}
