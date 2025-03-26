824912342f0fc994b0ee55b8894fbe407489babd
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-26 18:01:10 +0300

    ICinemaService interface'i oluşturuldu

--- Tickflix.Business/Abstract/ICinemaService.cs
+++ Tickflix.Business/Abstract/ICinemaService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tickflix.Models;
+﻿using Tickflix.Models;
 
 namespace Tickflix.Business.Abstract
 {
@@ -12,7 +7,7 @@ namespace Tickflix.Business.Abstract
         Cinema Add(Cinema cinema);
         Cinema Update(Cinema cinema);
         Cinema GetById(int id);
-        void Delete(int id);
+        void Delete(Cinema cinema);
         IQueryable<Cinema> GetAll();
     }
 }
