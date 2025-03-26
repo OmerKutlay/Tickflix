b8a011df971f272dbc5feb0ea141efb355d273f9
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-27 00:17:17 +0300

    Add CinemaService class for managing Cinema entities
    Introduce CinemaService class in Tickflix.Business.Concrete namespace.
    This class implements ICinemaService and provides methods for adding,
    deleting, retrieving, and updating Cinema objects using a repository
    pattern. Added necessary using directives for required namespaces.

---
+++ Tickflix.Business/Concrete/CinemaService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickflix.Business.Abstract;
+using Tickflix.Models;
+using Tickflix.Repository.Shared.Abstract;
+
+namespace Tickflix.Business.Concrete
+{
+    public class CinemaService : ICinemaService
+    {
+        private readonly IRepository<Cinema> _cinemaRepository;
+
+        public CinemaService(IRepository<Cinema> cinemaRepository)
+        {
+            _cinemaRepository = cinemaRepository;
+        }
+        public Cinema Add(Cinema cinema)
+        {
+            return _cinemaRepository.Add(cinema);
+        }
+
+        public void Delete(Cinema cinema)
+        {
+            _cinemaRepository.Delete(cinema);
+        }
+
+        public IQueryable<Cinema> GetAll()
+        {
+            return _cinemaRepository.GetAll();
+        }
+
+        public Cinema GetById(int id)
+        {
+            return _cinemaRepository.GetById(id);
+        }
+
+        public Cinema Update(Cinema cinema)
+        {
+            return (_cinemaRepository.Update(cinema));
+        }
+    }
+}
