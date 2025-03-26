ae04ab103d67eca80377927a8cd9128f36703991
Ömer Kutlay <omer.kutlay@outlook.com>
2025-03-26 18:36:14 +0300

    Refactor namespaces and add BusinessDI method
    Refactor namespaces to align with business logic structure:
    - Introduce using directives for `Tickflix.Business.Abstract` and `Tickflix.Business.Concrete`.
    - Change namespace from `Tickflix.Repository.Configuration` to `Tickflix.Business.Configuration`.
    
    Enhance dependency injection setup:
    - Add `BusinessDI` method in `BusinessExtension` class.
    - Register `IActorService`, `ICinemaService`, `IMovieService`, and `IProducerService` with `AddScoped`.
    - Adjust closing brace of `BusinessExtension` class to fit new method.

--- Tickflix.Business/Configuration/BusinessExtension.cs
+++ Tickflix.Business/Configuration/BusinessExtension.cs
@@ -4,13 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tickflix.Business.Abstract;
+using Tickflix.Business.Concrete;
 using Tickflix.Repository.Shared.Abstract;
 using Tickflix.Repository.Shared.Concrete;
 
-namespace Tickflix.Repository.Configuration
+namespace Tickflix.Business.Configuration
 {
     public static class BusinessExtension
     {
+        public static void BusinessDI(this IServiceCollection services)
+        {
+            services.AddScoped<IActorService, ActorService>();
+            services.AddScoped<ICinemaService, CinemaService>();
+            services.AddScoped<IMovieService, MovieService>();
+            services.AddScoped<IProducerService, ProducerService>();
+        }
 
         public static void RepositoryDI(this IServiceCollection services)
         {
