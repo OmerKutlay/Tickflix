f9ea31ef952244be53628389aea47ade8a04817c
OmerKutlay <omer.kutlay@outlook.com>
2025-04-30 22:19:29 +0300

    Add initial migration for Tickflix database schema
    This commit introduces the "Initial" migration for the Tickflix application. It creates the Actors, Cinemas, Producers, Movies, and ActorMovie tables, along with their relationships, foreign keys, and indexes. The model snapshot is also updated to reflect the new schema in the Entity Framework Core context.

--- Tickflix.Data/Migrations/20250429140359_Initial.Designer.cs
+++ Tickflix.Data/Migrations/20250429140359_Initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Tickflix.Web;
 
@@ -11,9 +12,11 @@ using Tickflix.Web;
 namespace Tickflix.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250429140359_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
