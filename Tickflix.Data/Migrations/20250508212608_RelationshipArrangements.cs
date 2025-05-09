6b010b54358a59bada70d2365755bdf3dd92e675
OmerKutlay <omer.kutlay@outlook.com>
2025-05-09 11:19:10 +0300

    Add RelationshipArrangements migration for EF Core
    This commit introduces a new migration named `RelationshipArrangements` for the Tickflix application. It creates a join table `ActorMovie` to establish a many-to-many relationship between `Actor` and `Movie` entities. The migration also defines the structure for the `Actor`, `Cinema`, `Movie`, and `Producer` entities, including their properties and relationships. The `Up` and `Down` methods are currently empty, indicating that the migration logic has not yet been implemented.

---
+++ Tickflix.Data/Migrations/20250508212608_RelationshipArrangements.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tickflix.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationshipArrangements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
