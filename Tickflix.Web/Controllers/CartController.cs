e168148650d2937bab6a62783ed590e714688818
OmerKutlay <omer.kutlay@outlook.com>
2025-05-28 21:23:08 +0300

    Add ICartService dependency to CartController
    Introduced a new dependency on the `Tickflix.Business.Abstract` namespace by adding the `ICartService` interface to the `CartController` class. A private readonly field `_cartService` is declared, and a constructor is added to initialize this field, enabling the controller to utilize the `ICartService` for its operations.

--- Tickflix.Web/Controllers/CartController.cs
+++ Tickflix.Web/Controllers/CartController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Tickflix.Business.Abstract;
 
 namespace Tickflix.Web.Controllers
 {
     public class CartController : Controller
     {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var cart = _cartService.GetCart();
+            ViewBag.Total = _cartService.GetCartTotal();
+            return View(cart.Items);
         }
+
+        [HttpPost]
+        public IActionResult Add(int movieId, int quantity = 1)
+        {
+            _cartService.AddToCart(movieId, quantity);
+            return RedirectToAction("Index");
     }
 }
