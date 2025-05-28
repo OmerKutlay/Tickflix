6a53ecad3135912d71260313943f6c99e75869da
OmerKutlay <omer.kutlay@outlook.com>
2025-05-28 21:23:43 +0300

    Add ICartService interface for cart management
    This commit introduces the `ICartService` interface in the
    `Tickflix.Business.Abstract` namespace. It provides methods
    for managing a shopping cart, including retrieving the cart,
    adding and removing items, clearing the cart, and calculating
    the total cost of the cart.

---
+++ Tickflix.Business/Abstract/ICartService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickflix.Models;
+
+namespace Tickflix.Business.Abstract
+{
+    public interface ICartService
+    {
+        Cart GetCart();
+        void AddToCart(int movieId, int quantity = 1);
+        void RemoveFromCart(int movieId);
+        void ClearCart();
+        double GetCartTotal();
+    }
+}
