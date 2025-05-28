cef844bc2118930d576b8896b45d8f9e05fbd9e8
OmerKutlay <omer.kutlay@outlook.com>
2025-05-28 21:24:38 +0300

    Add Cart class to manage shopping cart items
    Introduced a new `Cart` class in the `Tickflix.Models` namespace.
    This class includes a property `Items` that initializes a list
    of `CartItem` objects for managing items in the shopping cart.

---
+++ Tickflix.Models/Cart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickflix.Models
+{
+    public class Cart
+    {
+        public List<CartItem> Items { get; set; } = new List<CartItem>();
+    }
+}
