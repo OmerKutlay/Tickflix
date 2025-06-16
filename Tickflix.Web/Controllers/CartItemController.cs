916a7d2f7c1b368ba4851435f89ddeb060a8c06a
OmerKutlay <omer.kutlay@outlook.com>
2025-06-16 22:52:12 +0300

    CartItem Controller oluşturuldu

---
+++ Tickflix.Web/Controllers/CartItemController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tickflix.Web.Controllers
+{
+    public class CartItemController : Controller
+    {
+        private readonly ICartItemService _cartItemService;
+
+        public CartItemController(ICartItemService cartItemService)
+        {
+            _cartItemService = cartItemService;
+        }
+
+       
+    }
