0f4014c63a0e869f7ef17c652857646e2e386a1f
OmerKutlay <omer.kutlay@outlook.com>
2025-06-17 00:30:24 +0300

    Add CartItemService for managing CartItem objects
    Introduces the CartItemService class implementing the
    ICartItemService interface. This class provides methods for
    adding, deleting, retrieving, and updating CartItem instances.
    The GetAllByCartId method is fully implemented, while the
    other methods currently throw NotImplementedException.

---
+++ Tickflix.Business/Concrete/CartItemService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tickflix.Business.Abstract;
+using Tickflix.Models;
+using Tickflix.Repository.Shared.Abstract;
+
+namespace Tickflix.Business.Concrete
+{
+    public class CartItemService : ICartItemService
+    {
+        private readonly IRepository<CartItem> _cartItemRepository;
+        public CartItemService(IRepository<CartItem> cartItemRepository)
+        {
+            _cartItemRepository = cartItemRepository;
+        }
+        public CartItem Add(CartItem cartItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CartItem> GetAllByCartId(int cartId)
+        {
+           return _cartItemRepository.GetAll(ci => ci.CartId == cartId).ToList();
+        }
+
+        public CartItem Update(CartItem cartItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
