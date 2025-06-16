7d720b3c80bea15c580eeb1f0ad30adaf7bd119d
OmerKutlay <omer.kutlay@outlook.com>
2025-06-16 22:52:48 +0300

    CartItem Controller CRUD metotları yazıldı

--- Tickflix.Web/Controllers/CartItemController.cs
+++ Tickflix.Web/Controllers/CartItemController.cs
@@ -11,5 +11,33 @@ namespace Tickflix.Web.Controllers
             _cartItemService = cartItemService;
         }
 
-       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllByCartId(int cartId)
+        {
+            var cartItems = _cartItemService.GetAllByCartId(cartId);
+            return Ok(cartItems);
+        }
+
+        [HttpPost]
+        public IActionResult Add(CartItemController item)
+        {
+            return Ok(_cartItemService.Add(item));
+        }
+
+        [HttpPost]
+        public IActionResult Update(CartItemController item)
+        {
+            return Ok(_cartItemService.Update(item));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            return Ok(_cartItemService.Delete(id));
+        }
     }
